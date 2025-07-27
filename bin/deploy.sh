fb88d28bc80f6f96e397cdfd9740c51717de1838
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2025-07-27 14:37:39 +1000

    chore: increment patch version

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,4 @@
-docker build -f Dockerfile -t mattcoulter7/sqlite-backup-s3:latest -t mattcoulter7/sqlite-backup-s3:1.0.0 .
+docker build -f Dockerfile -t mattcoulter7/sqlite-backup-s3:latest -t mattcoulter7/sqlite-backup-s3:1.0.1 .
 docker login
-docker push mattcoulter7/sqlite-backup-s3:1.0.0
+docker push mattcoulter7/sqlite-backup-s3:1.0.1
 docker push mattcoulter7/sqlite-backup-s3:latest
